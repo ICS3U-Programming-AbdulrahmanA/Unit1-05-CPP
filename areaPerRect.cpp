05dedf7663e2035505f2d27eb08748fc27098393
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-09-15 18:07:12 +0000

    area and perimeter of rectangle in C++

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Abdul All rights reserved.
+//
+// Created by: Abdul
+// Date: 9/15/2025
+// Area and Perimeter of Rectangle
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "That has a length of 8.9 cm and a width of 6.7 cm: \n";
+    std::cout << "\n";
+    std::cout << "Area - " << 8.9 * 6.7 << " cm^2\n";
+    std::cout << "Perimeter - " << (2 * (8.9 + 6.7)) << "\n";
+}
